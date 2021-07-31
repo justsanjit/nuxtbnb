52ed71fb909fe1967819eace264c13503054c402
Sanjit <iamsanjit@hotmail.com>
2021-07-31 15:27:51 -0400

    Refactoring for google places

--- plugins/maps.client.js
+++ plugins/maps.client.js
@@ -1,45 +1,46 @@
 export default function(context, inject) {
-    let mapLoaded = false;
-    let mapWaiting = null;
-    inject('maps', {
-        showMap
-    })
-    addScript();
-    function addScript() {
-        const script = document.createElement('script')
-        script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB5eBV6JJFDtBzd2S6siCHl0IrkwyMWPlI&libraries=places&callback=initMap';
-        script.async = true
-        window.initMap = initMap
-        document.head.appendChild(script)
-    }
+  let isLoaded = false;
+  let waiting = [];
+  inject("maps", {
+    showMap
+  });
+  addScript();
+  function addScript() {
+    const script = document.createElement("script");
+    script.src =
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyB5eBV6JJFDtBzd2S6siCHl0IrkwyMWPlI&libraries=places&callback=initGoogleMaps";
+    script.async = true;
+    window.initGoogleMaps = initGoogleMaps;
+    document.head.appendChild(script);
+  }
 
-    function initMap() {
-        mapLoaded = true;
-        if (mapWaiting) {
-            const {canvas, lat, lng} = mapWaiting
-            renderMap(canvas, lat, lng)
-            mapWaiting = null
-        }
-    }
-
-    function showMap(canvas,lat, lng) {
-        if (mapLoaded) {
-            renderMap(canvas, lat, lng)
-        } else {
-            mapWaiting = {canvas, lat, lng}
-        }
-    }
-
-    function renderMap(canvas, lat, lng) {
-        const position = new window.google.maps.LatLng(lat, lng );
-        const mapOptions = {
-          zoom: 18,
-          center: position,
-          disableDefaultUI: true,
-          zoomControl: true,
-        };
-        const map = new window.google.maps.Map(canvas, mapOptions);
-        const marker = new window.google.maps.Marker({ position });
-        marker.setMap(map);
+  function initGoogleMaps() {
+    isLoaded = true;
+    waiting.forEach(item => {
+      if (typeof item.fn === "function") {
+        item.fn(...item.arguments);
       }
+    });
+    waiting = [];
+  }
+
+  function showMap(canvas, lat, lng) {
+    if (!isLoaded) {
+      waiting.push({
+        fn: showMap,
+        arguments
+      });
+      return;
+    }
+    const position = new window.google.maps.LatLng(lat, lng);
+    const mapOptions = {
+      zoom: 18,
+      center: position,
+      disableDefaultUI: true,
+      zoomControl: true
+    };
+    const map = new window.google.maps.Map(canvas, mapOptions);
+    const marker = new window.google.maps.Marker({ position });
+    marker.setMap(map);
+  }
 }
