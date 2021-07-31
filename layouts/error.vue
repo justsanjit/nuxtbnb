e528d061803032b307a7f174ef046a3618ae09e6
Sanjit <iamsanjit@hotmail.com>
2021-07-31 14:22:04 -0400

    Custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="text-align: center">
+    <h1>Error!</h1>
+    {{ error.message }} {{ error.statusCode }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: Object,
+    required: true,
+  },
+  head() {
+    return {
+      title: "Something went wrong!",
+    };
+  },
+};
+</script>
