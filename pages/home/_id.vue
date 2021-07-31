2c00a5c7db4175e33c551f2d81020e52cec7ec5c
Sanjit <iamsanjit@hotmail.com>
2021-07-31 01:17:43 -0400

    Creating a property page

---
+++ pages/home/_id.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div style="display: flex">
+      <img
+        v-for="image in home.images"
+        :src="image"
+        :key="image"
+        width="200"
+        height="150"
+      />
+    </div>
+    <div>{{ home.title }}</div>
+    {{ home.pricePerNight }} / night <br />
+    <img src="/images/marker.svg" widht="20" height="20" />
+    {{ home.location.address }} {{ home.location.city }}
+    {{ home.location.state }} {{ home.location.country }}<br />
+    <img src="/images/star.svg" widht="20" height="20" />
+    {{ home.reviewValue }} <br />
+    {{ home.guests }} guests, {{ home.beds }} beds,
+    {{ home.bathrooms }} bathrooms <br />
+  </div>
+</template>
+
+<script>
+import homes from "~/data/homes.json";
+export default {
+  head() {
+    return {
+      title: this.home.title,
+    };
+  },
+  data() {
+    return {
+      home: null,
+    };
+  },
+  created() {
+    this.home = homes.find((home) => home.objectID === this.$route.params.id);
+  },
+};
+</script>
